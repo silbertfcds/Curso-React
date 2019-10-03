076469236c142c6f3fde67787ffdde5b745ff94b
Silbert <silbert.fcds@hotmail.com>
2019-10-03 18:21:36 -0300

    Listando produtos

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
-
+import './styles.css';
 
 export default class Main extends Component {
     state = {
@@ -22,7 +22,12 @@ export default class Main extends Component {
         return (
             <div className="product-list">
                 {this.state.products.map(product =>(
-                    <h2 key={product._id}>{product.title}</h2>
+                    <article key={product._id}>
+                        <strong>{product.title}</strong>
+                        <p>{product.description}</p>
+
+                        <a href="">Acessar</a>
+                    </article>
                 ))}
             </div>
         );
